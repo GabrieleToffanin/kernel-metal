23db93de160ea042c3ffc7248017157d71ceeef6
Gabriele Toffanin <GabrieleToffanin@outlook.it>
2024-08-05 20:09:51 +0200

    chore: Add configuration files for building the kernel

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,42 @@
-#![no_std]
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
 
-fn panic(_info: &PanicInfo) -> ! {
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-
-
-fn main() {
-    println!("Hello, world!");
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+    
+    #[cfg(test)]
+    test_main();
+    
+    loop {}
 }
+
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+
