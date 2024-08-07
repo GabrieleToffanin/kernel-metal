2a5fb2b62c1c127c782b3f63b9e44d648b0d298b
Gabriele Toffanin <GabrieleToffanin@outlook.it>
2024-08-07 14:24:58 +0200

    feat: Add breakpoint handler

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # kernel-metal
 Trying to understeand how to implement an OS from scratch
+
+## This is a simple run with a debug point
+
+![alt text](image.png)
+
+
--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,17 @@ use blog_os::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    blog_os::init(); // new
+
+    // invoke a breakpoint exception
+    // Breakpoints MAGIC solved!
+    x86_64::instructions::interrupts::int3(); // new
+
+    // as before
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
